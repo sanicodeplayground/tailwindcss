1eaa389d3a2b29dcec21eb0ad2f7202831968dc4
sanicodeplayground <106916427+sanicodeplayground@users.noreply.github.com>
2024-01-10 16:03:06 +0000

    custom colour added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#a855f7",
+      },
+    },
   },
   plugins: [],
 };
